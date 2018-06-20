4267740ce581613248d268d45fccee3bfc11393b
manviagarwal998 <39941237+manviagarwal998@users.noreply.github.com>
2018-06-20 13:54:39 +0530

    Add files via upload
    TEXT_T0_SPEECH with espeak and os

---
+++ text_to_speech.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#library espeak for text to speech translation
+from espeak import espeak
+espeak.synth('hi,drishti')
+
+#library os library espeak for text to speech translation
+import os
+os.system("espeak 'the'")
+
